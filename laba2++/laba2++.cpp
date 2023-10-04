0516ff8514fd7f562daad31cafa55f6dfb1f7315
Вася <@DESKTOP-SFOGOU7>
2023-10-04 10:38:17 +0300

    Добавьте файлы проекта.

---
+++ laba2++/laba2++.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int max_num();
+void print(int max)
+{
+    cout << max;
+}
+
+int main()
+{
+    int max = max_num();
+    print(max);
+    system("pause");
+    return 0;
+}
+
+int max_num()
+{
+    int x, y;
+    cin >> x >> y;
+    if (x >= y)
+    {
+        return x;
+    }
+    else
+    return y;
+}
